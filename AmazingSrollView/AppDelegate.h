1769418bf5c5ebb5314ca173056e6cb4e6440545
hzcly98 <hzcly982015@163.com>
2017-10-24 18:03:31 +0800

    Initial Commit

---
+++ AmazingSrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AmazingSrollView
+//
+//  Created by hzc on 2017/10/24.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
